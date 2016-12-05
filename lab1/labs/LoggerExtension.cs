135868a759665c8ad093ab67747af046ea6e685c
Evgeny74 <ifaydaen@mail.ru>
2016-12-05 14:00:31 +0500

    Added documentation to LoggerExtension

--- lab1/labs/LoggerExtension.cs
+++ lab1/labs/LoggerExtension.cs
@@ -3,8 +3,16 @@ using CarsLibrary;
 
 namespace labs
 {
+    /// <summary>
+    /// Класс-расширение класса Logger
+    /// </summary>
     public static class LoggerExtension
     {
+        /// <summary>
+        /// Метод логирования методов расширений коллекции
+        /// </summary>
+        /// <param name="logger">Сам логер</param>
+        /// <param name="message">Сообщения для логирования</param>
         public static void Log(this Logger logger,string message)
         {
             logger.getOutput().WriteLine($"{DateTime.Now}: {message}");
